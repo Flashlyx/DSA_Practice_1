c998a0b6e8f0a53f05b5b4cf3e3f2a434c6c40f9
Flashlyx <jayeesh2003@gmail.com>
2025-10-30 00:00:23 +0530

    concept prefix sum problem - pivot index ! done with the BF solution.

---
+++ src/Pivot_Index/PivotIndexBF.java
@@ -0,0 +1,28 @@
+package Pivot_Index;
+
+public class PivotIndexBF {
+    public static int pivotIndexBF(int[] nums) {
+
+        for(int i = 0; i < nums.length; i++){
+
+            int leftSum = 0;
+            for(int x = 0; x < i; x++){
+                leftSum = leftSum + nums[x];
+            }
+
+            int rightSum = 0;
+            for(int y = i + 1; y < nums.length; y++){
+                rightSum = rightSum + nums[y];
+            }
+            if(leftSum == rightSum){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String [] args){
+        int[] nums1 = {1,7,3,6,5,6};
+        System.out.println(pivotIndexBF(nums1));
+    }
+}
