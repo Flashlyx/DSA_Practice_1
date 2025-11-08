6f0a88564ab340780198b0405999e7325c8850ef
Flashlyx <jayeesh2003@gmail.com>
2025-11-09 00:04:14 +0530

    added a new test case to our 'two sum' problem that returns the IllegalArgumentException as we specified.

--- src/TwoSum/TwoSumOpt.java
+++ src/TwoSum/TwoSumOpt.java
@@ -9,7 +9,7 @@ public class TwoSumOpt {
     public static int[] twoSumOpt(int[] nums, int target){
         Map<Integer, Integer> numMap= new HashMap<>();
 
-        for (int i = 0; i <= nums.length; i++){
+        for (int i = 0; i < nums.length; i++){
 
             int pair = target - nums[i];
 
@@ -24,7 +24,14 @@ public class TwoSumOpt {
         int[] nums1 = {4,2,6,5,1};
         int target1 = 11;
 
-        int[] res = twoSumOpt(nums1, target1);
-        System.out.println(Arrays.toString(res));
+        int[] res1 = twoSumOpt(nums1, target1);
+        System.out.println(Arrays.toString(res1));
+
+        int[] nums2 = {5,3,2,10,10};
+        int target2 = 20;
+        int[] res2 = twoSumOpt(nums1,target2);
+        System.out.println(Arrays.toString(res2));
+
+
     }
 }
