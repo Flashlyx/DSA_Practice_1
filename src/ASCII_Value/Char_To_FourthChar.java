efa9e9b91977c6ab4c18c628244a7965563411ca
Flashlyx <jayeesh2003@gmail.com>
2025-11-20 11:05:43 +0530

    learned to convert char to int and vice versa using ASCII values by typecasting. this program allows to convert given string's chars to its nth char value.

---
+++ src/ASCII_Value/Char_To_FourthChar.java
@@ -0,0 +1,26 @@
+package ASCII_Value;
+
+public class Char_To_FourthChar {
+    public static String toNthChar(String str, int n){
+
+        StringBuilder res = new StringBuilder();
+
+
+        for (int i = 0; i < str.length();i++){
+
+            char ch = str.charAt(i);
+            char nthChar = (char) (ch + (n));
+            res.append(nthChar);
+
+
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(toNthChar("hello",4));
+
+        System.out.println(toNthChar("jayeesh",3));
+    }
+}
