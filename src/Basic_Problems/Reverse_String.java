a011765aa0fc307866e195323ada9d0bcf013d4c
Flashlyx <jayeesh2003@gmail.com>
2025-11-23 16:34:01 +0530

    added another method that does not use inbuilt method(like charAt() to reverse string

--- src/Basic_Problems/Reverse_String.java
+++ src/Basic_Problems/Reverse_String.java
@@ -1,5 +1,7 @@
 package Basic_Problems;
 
+import java.util.Arrays;
+
 public class Reverse_String {
     public static String reverseString(String str){
         StringBuilder sb = new StringBuilder();
@@ -12,9 +14,29 @@ public class Reverse_String {
         return res;
     }
 
+    public static String reverseString2(String str){
+
+
+        char[] sarr = str.toCharArray();
+        int left = 0 , right = sarr.length - 1;
+
+        while (left < right){
+
+            char temp = sarr[left];
+            sarr[left] = sarr[right];
+            sarr[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(sarr);
+    }
+
     public static void main(String[] args) {
 
         System.out.println(reverseString("jayeesh"));
         System.out.println(reverseString("samuel"));
+
+        System.out.println(reverseString2("Mehtod"));
     }
 }
