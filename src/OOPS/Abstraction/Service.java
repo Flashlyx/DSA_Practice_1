2fe4e98cfe453c7a621b06b8f0123bd6520b8db6
Flashlyx <jayeesh2003@gmail.com>
2025-12-04 00:17:36 +0530

    Implemented an example program for an OOPs pillar Abstraction by creating an Interface !

---
+++ src/OOPS/Abstraction/Service.java
@@ -0,0 +1,13 @@
+package OOPS.Abstraction;
+
+public interface Service {
+
+    int balance = 1000;
+
+    void greetUser(String userName);
+
+    static void message(){
+        System.out.println("Welcome");
+    }
+}
+
