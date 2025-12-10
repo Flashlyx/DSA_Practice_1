a600e99cddc70c07acaeb9905fc53f92a56df49c
Flashlyx <jayeesh2003@gmail.com>
2025-12-10 22:18:27 +0530

    creating a thread by implementing runnable interface !

---
+++ src/Threads_Learning/Sample_Implements_Thread.java
@@ -0,0 +1,14 @@
+package Threads_Learning;
+
+class MyThread2 implements Runnable{
+    public void run(){
+        System.out.println(" My Thread 2 is running !!! ");
+    }
+}
+public class Sample_Implements_Thread {
+    public static void main(String[] args) {
+
+        Thread mt2 = new Thread(new MyThread2());
+        mt2.start();
+    }
+}
